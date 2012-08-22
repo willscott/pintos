2043c1bba5136f513680a11142ab46b49928057d
Will Scott <willscott@gmail.com>
2012-08-22 10:41:51 -0700

    Added csr structure.  Perform initial software reset on device.

--- src/devices/e100.c
+++ src/devices/e100.c
@@ -1,6 +1,17 @@
 #include "devices/e100.h"
+#include "threads/io.h"
 
 int pci_e100_attach(struct pci_func *pcif)
 {
-  printf("e100 device detected.\n");
+  printf("E100: Device found.\n");
+  pci_func_enable(pcif);
+  struct e100_csr* e100 = (struct e100_csr*)pcif->reg_base[1];
+  printf("E100: iobase=0x%x\n", pcif->reg_base[1]);
+  software_reset(e100);
+  return 0;
+}
+
+void software_reset(struct e100_csr *e100)
+{
+  outl(&e100->port, 0x0);
 }
--- src/devices/e100.h
+++ src/devices/e100.h
@@ -9,4 +9,21 @@
 
 int pci_e100_attach(struct pci_func *pcif);
 
+struct e100_csr {
+  uint16_t          scb_cmd;
+  volatile uint16_t scb_status;
+  uint32_t          scb_ptr;
+  uint32_t          port;
+  uint16_t          eeprom;
+  uint16_t          prvt_1;
+  uint32_t          mdi;
+  uint32_t          rx_dma_cnt;
+  uint8_t           pmdr;
+  uint16_t          flow_ctrl;
+  uint8_t           prvt_2;
+  uint16_t          prvt_3;
+  uint8_t           status;
+  uint8_t           ctrl;
+};
+
 #endif // DEVICES_E100_H
