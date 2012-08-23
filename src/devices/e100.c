65ed2f77bbfcda03691fce655d10fb4cfea11e09
Will Scott <willscott@gmail.com>
2012-08-23 11:33:12 -0700

    formalized e100 data structures

--- src/devices/e100.c
+++ src/devices/e100.c
@@ -1,17 +1,29 @@
 #include "devices/e100.h"
+#include "devices/timer.h"
 #include "threads/io.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
+
+void pci_e100_init(void *e100);
 
 int pci_e100_attach(struct pci_func *pcif)
 {
   printf("E100: Device found.\n");
   pci_func_enable(pcif);
   struct e100_csr* e100 = (struct e100_csr*)pcif->reg_base[1];
-  printf("E100: iobase=0x%x\n", pcif->reg_base[1]);
-  software_reset(e100);
+
+  // Create a kernel thread for managing the device.
+  thread_create("E100 Manager", PRI_DEFAULT, &pci_e100_init, e100);
   return 0;
 }
 
-void software_reset(struct e100_csr *e100)
+void pci_e100_init(void *csr)
 {
-  outl(&e100->port, 0x0);
+  struct e100_csr* e100 = (struct e100_csr*)csr;
+  // Software reset.  Takes up to 10us to complete
+  outl((uint16_t)&e100->port, 0x0);
+  timer_sleep(10);
+
+  //printf("E100: Initializing Transmit Buffer.\n");
+  //void* tx_loc = malloc(E100_TX_RING_SIZE * sizeof());
 }
--- src/devices/e100.h
+++ src/devices/e100.h
@@ -7,23 +7,59 @@
 #define E100_QEMU_DEVICEID 0x1209
 #define E100_BOCHS_DEVICEID 0x100e
 
+#define E100_RX_RING_SIZE = 0x100
+#define E100_TX_RING_SIZE = 0x100
+
 int pci_e100_attach(struct pci_func *pcif);
 
+// Control / Status Register layout.
 struct e100_csr {
-  uint16_t          scb_cmd;
   volatile uint16_t scb_status;
+  uint16_t          scb_cmd;
   uint32_t          scb_ptr;
   uint32_t          port;
-  uint16_t          eeprom;
   uint16_t          prvt_1;
+  uint16_t          eeprom;
   uint32_t          mdi;
   uint32_t          rx_dma_cnt;
-  uint8_t           pmdr;
-  uint16_t          flow_ctrl;
   uint8_t           prvt_2;
-  uint16_t          prvt_3;
-  uint8_t           status;
+  uint16_t          flow_ctrl;
+  uint8_t           pmdr;
   uint8_t           ctrl;
+  uint8_t           status;
+  uint16_t          prvt_3;
 };
+
+// Command Block layout.
+struct e100_cb {
+  volatile uint16_t status;
+  uint16_t          cmd;
+  uint32_t          address;
+};
+
+// Transmit Buffer Descriptor.
+struct e100_tbd {
+  uint32_t          address;
+  uint16_t          size;
+  uint16_t          eol;
+};
+
+// Transmit command block.
+struct e100_tcb {
+  struct e100_cb    cb;
+  struct e100_tbd*  tbd;
+  uint16_t          size;
+  uint8_t           threshold;
+  uint8_t           tbd_count;
+};
+
+// Receive Frame Descriptor.
+struct e100_rfd {
+  struct e100_cb    cb;
+  uint32_t          reserved;
+  uint16_t          count;
+  uint16_t          size;
+};
+
 
 #endif // DEVICES_E100_H
