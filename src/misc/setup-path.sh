223506d13baa119f9b055de4af4dea0bafccd4be
Adam Lerner <eletari@gmail.com>
2012-09-20 18:01:46 -0700

    Added setup-path.sh to add symlinks to pintos binary, etc. in path

---
+++ src/misc/setup-path.sh
@@ -0,0 +1,4 @@
+ln -s `pwd`/../utils/pintos /usr/local/bin/pintos
+ln -s `pwd`/../utils/pintos-dbg /usr/local/bin/pintos-dbg
+ln -s `pwd`/../utils/pintos-mkdisk /usr/local/bin/pintos-mkdisk
+ln -s `pwd`/../utils/Pintos.pm /usr/local/bin/Pintos.pm
