c20f1706553bf09308816a96ea496f013f6917db
Will Scott <willscott@gmail.com>
2012-09-21 14:00:39 -0700

    get path setup to work on lab machines

--- src/misc/setup-path.sh
+++ src/misc/setup-path.sh
@@ -1,4 +1,3 @@
-sudo ln -s `pwd`/../utils/pintos /usr/local/bin/pintos
-sudo ln -s `pwd`/../utils/pintos-dbg /usr/local/bin/pintos-dbg
-sudo ln -s `pwd`/../utils/pintos-mkdisk /usr/local/bin/pintos-mkdisk
-sudo ln -s `pwd`/../utils/Pintos.pm /usr/local/bin/Pintos.pm
+cd ../utils
+make
+echo "PATH=`pwd`:\$PATH" >> ~/.bashrc
