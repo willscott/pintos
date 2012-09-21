c3454974871edd9663b90eeeb85501872f4e872a
Adam Lerner <eletari@gmail.com>
2012-09-20 18:07:00 -0700

    Sudo in setup-path.sh

--- src/misc/setup-path.sh
+++ src/misc/setup-path.sh
@@ -1,4 +1,4 @@
-ln -s `pwd`/../utils/pintos /usr/local/bin/pintos
-ln -s `pwd`/../utils/pintos-dbg /usr/local/bin/pintos-dbg
-ln -s `pwd`/../utils/pintos-mkdisk /usr/local/bin/pintos-mkdisk
-ln -s `pwd`/../utils/Pintos.pm /usr/local/bin/Pintos.pm
+sudo ln -s `pwd`/../utils/pintos /usr/local/bin/pintos
+sudo ln -s `pwd`/../utils/pintos-dbg /usr/local/bin/pintos-dbg
+sudo ln -s `pwd`/../utils/pintos-mkdisk /usr/local/bin/pintos-mkdisk
+sudo ln -s `pwd`/../utils/Pintos.pm /usr/local/bin/Pintos.pm
